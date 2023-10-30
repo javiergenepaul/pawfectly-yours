9cff89cb98286e453193eed4ebcae6890bb061fc
gjavier <gjavier@csyoutsourcing.com>
2023-10-30 13:22:59 +0800

    add temprary middleware

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+import React from "react";
+
+export default function middleware(request: NextRequest) {
+  const cookie = request.cookies.get("auth")?.valueOf();
+  console.log("cookie", cookie);
+
+  return NextResponse.redirect(new URL("/login", request.url));
+}
+
+export const config = {
+  matcher: "/",
+};
